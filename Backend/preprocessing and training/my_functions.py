02398f5852d60b5b53e285b66bc1d03bf59fd44d
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2022-01-30 14:07:44 +0530

    preprocessing image

---
+++ Backend/preprocessing and training/my_functions.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+
+def biggest_contour(contours):
+    biggest = np.array([])
+    max_area = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 50:
+            peri = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            if area > max_area and len(approx) == 4:
+                biggest = approx
+                max_area = area
+    return biggest, max_area
+
+
+def show_my_image(my_img):
+    cv2.imshow("image", my_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def show_my_small_image(my_img):
+    show_my_image(cv2.resize(my_img, (0, 0), fx=0.5, fy=0.5))
+
+
+def reorder(my_points):
+    my_points = my_points.reshape((4, 2))
+    my_points_new = np.zeros((4, 1, 2), dtype=np.int32)
+    add = my_points.sum(1)
+    my_points_new[0] = my_points[np.argmin(add)]
+    my_points_new[3] = my_points[np.argmax(add)]
+    diff = np.diff(my_points, axis=1)
+    my_points_new[1] = my_points[np.argmin(diff)]
+    my_points_new[2] = my_points[np.argmax(diff)]
+    return my_points_new
